707fd322110e6513c1a30f20f163be4147326723
Maxim <mbzozn@mail.ru>
2021-06-10 14:08:50 +0300

    Files Added

---
+++ ASPNET001/ASPNET001/Controllers/AgentsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult RegisterAgent([FromBody] IAgentInfo agentInfo)
+        {
+            return Ok();
+        }
+        [HttpPut("enable/{agentId}")]
+        public IActionResult EnableAgentById([FromRoute] int agentId )
+        {
+            return Ok();
+        }
+        [HttpPut("disable/{agentId}")]
+        public IActionResult DisableAgentById([FromRoute] int agentId)
+        {
+            return Ok();
+        }
+    }
+}

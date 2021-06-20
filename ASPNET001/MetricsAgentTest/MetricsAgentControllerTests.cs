5433fca104bc8c0757b3e4f2a99f5278a0127989
Maxim <mbzozn@mail.ru>
2021-06-20 13:46:56 +0300

    New Changes

--- ASPNET001/MetricsAgentTest/MetricsAgentControllerTests.cs
+++ ASPNET001/MetricsAgentTest/MetricsAgentControllerTests.cs
@@ -1,10 +1,11 @@
+using MetricsAgent;
 using MetricsAgent.Controllers;
-using MetricsManager;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
 namespace MetricsAgentTest
-{/*
+{
     public class CpuMetricsControllerUnitTests
     {
         private CpuMetricsController cpu—ontroller;
@@ -19,22 +20,96 @@ namespace MetricsAgentTest
         {
             var fromTime = DateTimeOffset.Now;
             var toTime = DateTimeOffset.UtcNow;
-
+           
             var result = cpu—ontroller.GetCpuMetrics(fromTime, toTime);
-
+            
             _ = Assert.IsAssignableFrom<IActionResult>(result);
         }
+    }
 
+    public class DotNetMetricsControllerUnitTests
+    {
+        private DotNetMetricsController dotnet—ontroller;
+
+        public DotNetMetricsControllerUnitTests()
+        {
+            dotnet—ontroller = new DotNetMetricsController();
+        }
 
         [Fact]
-        public void GetMetricsFromAllClusterFrist_ReturnsOk()
+        public void GetMetricsFromErrorsCount_ReturnsOk()
         {
             var fromTime = DateTimeOffset.Now;
             var toTime = DateTimeOffset.UtcNow;
 
-            var result = cpu—ontroller.GetMetricsFromAllCluster(fromTime, toTime);
+            var result = dotnet—ontroller.GetErrorsCount(fromTime, toTime);
 
             _ = Assert.IsAssignableFrom<IActionResult>(result);
         }
-    }*/
+    }
+
+    public class HddMetricsControllerUnitTests
+    {
+        private HddMetricsController hdd—ontroller;
+
+        public HddMetricsControllerUnitTests()
+        {
+            hdd—ontroller = new HddMetricsController();
+        }
+
+        [Fact]
+        public void GetGetLeftSpace_ReturnsOk()
+        {
+            var fromTime = DateTimeOffset.Now;
+            var toTime = DateTimeOffset.UtcNow;
+
+            var result = hdd—ontroller.GetLeftSpace(fromTime, toTime);
+
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+
+    public class NetworkMetricsControllerUnitTests
+    {
+        private NetworkMetricsController network—ontroller;
+
+        public NetworkMetricsControllerUnitTests()
+        {
+            network—ontroller = new NetworkMetricsController();
+        }
+
+
+        [Fact]
+        public void GetGetLeftSpace_ReturnsOk()
+        {
+            var fromTime = DateTimeOffset.Now;
+            var toTime = DateTimeOffset.UtcNow;
+
+            var result = network—ontroller.GetNetworkBandwidth(fromTime, toTime);
+
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+
+    public class RamMetricsControllerUnitTests
+    {
+        private RamMetricsController ram—ontroller;
+
+        public RamMetricsControllerUnitTests()
+        {
+            ram—ontroller = new RamMetricsController();
+        }
+
+
+        [Fact]
+        public void GetGetLeftSpace_ReturnsOk()
+        {
+            var fromTime = DateTimeOffset.Now;
+            var toTime = DateTimeOffset.UtcNow;
+
+            var result = ram—ontroller.GetAvailableSpace(fromTime, toTime);
+
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
 }
